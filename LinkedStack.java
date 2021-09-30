0aa11ee8dba4856f6218b1477678b8aeb16cd509
JohnSalinas123 <salinasjohn257@gmail.com>
2021-09-29 19:41:40 -0700

    initial setup with the use of give source code

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,3 +1,92 @@
-public class LinkedStack {
+import java.util.EmptyStackException;
+
+/**
+ * 
+ */
+
+public final class LinkedStack<T> implements StackInterface<T>
+{
+	private Node topNode; // References the first node in the chain
+  
+   public LinkedStack()
+   {
+      topNode = null;
+   } // end default constructor
+
+
+	private class Node
+	{
+      private T    data; // Entry in stack
+      private Node next; // Link to next node
+      
+      private Node(T dataPortion)
+      {
+         this(dataPortion, null);
+      } // end constructor
+      
+      private Node(T dataPortion, Node linkPortion)
+      {
+         data = dataPortion;
+         next = linkPortion;
+      } // end constructor
+      
+      private T getData()
+      {
+         return data;
+      } // end getData
+      
+      private void setData(T newData)
+      {
+         data = newData;
+      } // end setData
+      
+      private Node getNextNode()
+      {
+         return next;
+      } // end getNextNode
+      
+      private void setNextNode(Node nextNode)
+      {
+         next = nextNode;
+      } // end setNextNode
+	} // end Node
+
+   //  < Implementations of the stack operations go here. >
+
+    @Override
+    public void push(T newEntry) {
+         Node newNode = new Node(newEntry, topNode);
+         topNode = newNode;
+        
+    } // end push
+
+    @Override
+    public T pop() {
+         T top = peek(); // Might throw EmptyStackException
+         // Assertion: topNode != null;
+         topNode = topNode.getNextNode();
+        return top;
+    } // end pop
+
+    @Override
+    public T peek() {
+         if (isEmpty()) {
+            throw new EmptyStackException();
+         } else {
+            return topNode.getData();
+         }
+    } // end peek
+
+    @Override
+    public boolean isEmpty() {
+         return topNode == null;
+    } // end isEmpty
+
+    @Override
+    public void clear() {
+      topNode = null;
+    } // end clear
+
     
-}
+
+} // end LinkedStack
--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -1,3 +1,113 @@
-public class ResizeableArrayStack {
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * 
+ */
+
+public final class ResizeableArrayStack<T> implements StackInterface<T>
+{
+    private T[] stack;    // Array of stack entries
+    private int topIndex; // Index of top entry
+    private boolean integrityOK = false;
+    private static final int DEFAULT_CAPACITY = 50;
+    private static final int MAX_CAPACITY = 10000;
+  
+   public ResizeableArrayStack()
+   {
+      this(DEFAULT_CAPACITY);
+   } // end default constructor
+  
+   public ResizeableArrayStack(int initialCapacity)
+   {
+        integrityOK = false;
+        checkCapacity(initialCapacity);
+        
+        // The cast is safe because the new array contains null entries
+        @SuppressWarnings("unchecked")
+        T[] tempStack = (T[])new Object[initialCapacity];
+        stack = tempStack;
+        topIndex = -1;
+        integrityOK = true;
+  } // end constructor
+
+    //  < Implementations of the private methods go here; checkCapacity and checkIntegrity
+    //    are analogous to those in Chapter 2. >
+
+    private void checkCapacity(int capacity) {
+        if (capacity > MAX_CAPACITY) {
+            throw new IllegalStateException("Attempt to create a bag whose " +
+                                            "capacity exceeds allowed " +
+                                            "maximum of " + MAX_CAPACITY);
+        }
+
+    } // end checkCapacity
+
+    private void ensureCapacity() {
+        if (topIndex >= stack.length - 1) {
+            int newLength = 2 * stack.length;
+            checkCapacity(newLength);
+            stack = Arrays.copyOf(stack, newLength);
+        }
+    } // end ensureCapacity
+
+    private void checkIntegrity() {
+        if (!integrityOK) {
+            throw new SecurityException("ArrayStack object is corrupt");
+        }
+    } // end checkIntegrity
+
+    //  < Implementations of the stack operations go here. >
+
+    @Override
+    public void push(T newEntry) {
+        checkIntegrity();
+        ensureCapacity();
+        stack[topIndex + 1] = newEntry;
+        topIndex++;
+    } // end push
+
+    @Override
+    public T pop() {
+        checkIntegrity();
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        } else {
+            T top = stack[topIndex];
+            stack[topIndex] = null;
+            topIndex--;
+            return top;
+        }
+    } // end pop 
+
+    @Override
+    public T peek() {
+        checkIntegrity();
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        } else {
+            return stack[topIndex];
+        }
+    } // end peek
+
+    @Override
+    public boolean isEmpty() {
+        return topIndex < 0;
+    } // end isEmpty
+
+    @Override
+    public void clear() {
+        checkIntegrity();
+
+        // Remove refferences to the objects in the stack
+        // but do not deallocate the array
+        while (topIndex > -1) {
+            stack[topIndex] = null;
+            topIndex--;
+        }
+        // Assertion: topIndex is -1
+    } // end clear
+
     
-}
+    
+} // end ArrayStack
