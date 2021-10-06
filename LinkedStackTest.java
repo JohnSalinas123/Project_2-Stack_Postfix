faad40becf2bf7558d463597c11794b48c2f6c55
JohnSalinas123 <salinasjohn257@gmail.com>
2021-10-06 01:57:12 -0700

    added some simple testing code

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,3 +1,15 @@
 public class LinkedStackTest {
     
+    public static void main(String[] args) {
+
+        String infixStr = "a-b+c";
+
+        StackInterface<Character> stack1 = new LinkedStack<>();
+
+        System.out.print("Result: ");
+        System.out.println(stack1.convertToPostfix(infixStr));
+
+
+    }
+
 }
