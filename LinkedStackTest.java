4dc8223cac8a3af91d7c1cecd280b5d44b2d5106
JohnSalinas123 <salinasjohn257@gmail.com>
2021-10-10 14:11:35 -0700

    updated tests to show input and output of convertToPostfix

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,13 +1,27 @@
 public class LinkedStackTest {
     
     public static void main(String[] args) {
+        /**
+         * Demo the functionality of the algorithm convertToPostfix
+         */
 
-        String infixStr = "a-b+c";
-
+        // creating two stacks
         StackInterface<Character> stack1 = new LinkedStack<>();
+        StackInterface<Character> stack2 = new LinkedStack<>();
 
-        System.out.print("Result: ");
-        System.out.println(stack1.convertToPostfix(infixStr));
+        // infix expressions to be converted to postfix for demo
+        String infix1 = "a/b*(c+(d-e))"; 
+        String infix2 = "a*b/(c-a)+d*e";
+
+        // changing result of converToPostfix to string
+        String postfix1 = String.valueOf(stack1.convertToPostfix(infix1));
+        String postfix2 = String.valueOf(stack2.convertToPostfix(infix2));
+
+        // outputing results formated to show the starting input and the output of algorithm
+        System.out.println("Coverting infix to postfix: \n");
+        System.out.println("Infix: " + infix1 + " ->  Postfix: " + postfix1 + "\n");
+        System.out.println("Infix: " + infix2 + " ->  Postfix: " + postfix2 + "\n");
+       
 
 
     }
