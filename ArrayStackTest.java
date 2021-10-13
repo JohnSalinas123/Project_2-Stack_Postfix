001135189953fa4423d277e5177e8e7f3d0c640d
Priscilla Ballesteros <89608053+PBallesteros02@users.noreply.github.com>
2021-10-12 20:00:31 -0700

    fixed code

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -1,12 +1,75 @@
 public class ArrayStackTest {
+
+    public static double evaluePostfix(String postfixString, int[ ] valueOfVar ) {
+       
+        StackInterface<Integer> valueStackInterface = new ResizeableArrayStack<>() ;
+            
+            char nextItem ;
+            int value = 0 ;
+            int operand1 ;
+            int operand2 ;
+            int result = 0 ;
+            int i = 0 ;
+    
+            while(i < postfixString.length()){
+                nextItem = postfixString.charAt(i);
+                if (charVariable(nextItem)){
+                    switch(nextItem){
+                        case'a':
+                            value = valueOfVar[0];
+                            break;
+                        case'b':
+                            value = valueOfVar[1];
+                            break;
+                        case'c':
+                            value = valueOfVar[2];
+                            break;
+                        case'd':
+                            value = valueOfVar[3];
+                            break;
+                        case'e':
+                            value = valueOfVar[4];
+                            break;
+                        default:
+                            break; 
+                
+                    } 
+                        valueStackInterface.push(value); 
+                } else {
+                    operand1 = valueStackInterface.pop();
+                    operand2 = valueStackInterface.pop();
+                    switch(nextItem){
+                        case '+':result = operand1 + operand2;
+                        break;
+                        case '*':result = operand1 * operand2;
+                            break;
+                        case '-':result = operand1 - operand2;
+                            break;
+                        case '/':result = operand1 / operand2;
+                            break;
+                            default:break;
+                    }
+                    valueStackInterface.push(result);
+                }
+                i++ ;
+    
+            }
+            return valueStackInterface.pop();
+        }
+        private static boolean charVariable(char ch){
+            return(ch == 'a') || (ch == 'b') || (ch == 'c') || (ch == 'd') || (ch == 'e');
+        }
+
+
+
     
     public static void main(String[] args) {
 
-        String infixStr = "a-b+c";
+        int[] numbers =  { 2,3,4,5,6};
 
-        StackInterface<Character> stack1 = new ResizeableArrayStack<>() ;
+        String postfixStr = "a-b+c";
 
         System.out.print("Result: ");
-        System.out.println(stack1.evaluePostfix(postfixString));  
+        System.out.println(evaluePostfix(postfixStr,numbers));  
     }
 }
--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 import java.util.EmptyStackException;
 
 /**
- * Stack implemented using a resizeable array.
+ * 
  */
+
 public final class ResizeableArrayStack<T> implements StackInterface<T>
 {
     private T[] stack;    // Array of stack entries
@@ -30,11 +31,9 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>
         integrityOK = true;
   } // end constructor
 
-    
-    /** Checks whether the capacity of the stack has exceeded the max capacity, if so
-     * an IllegalStateException is thrown.
-     * @param capacity The capacity of the stack.
-     */
+    //  < Implementations of the private methods go here; checkCapacity and checkIntegrity
+    //    are analogous to those in Chapter 2. >
+
     private void checkCapacity(int capacity) {
         if (capacity > MAX_CAPACITY) {
             throw new IllegalStateException("Attempt to create a bag whose " +
@@ -44,11 +43,6 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>
 
     } // end checkCapacity
 
-    /**
-     * Ensures that there is enough capacity in the stack, if not it makes 
-     * a new stack
-     * with double the capacities.
-     */
     private void ensureCapacity() {
         if (topIndex >= stack.length - 1) {
             int newLength = 2 * stack.length;
@@ -57,20 +51,16 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>
         }
     } // end ensureCapacity
 
-    /**
-     * Checks the integrity of the stack, if not then a SecurityException
-     * is thrown.
-     */
     private void checkIntegrity() {
         if (!integrityOK) {
             throw new SecurityException("ArrayStack object is corrupt");
         }
     } // end checkIntegrity
 
-    
-    /** Adds newEntry to the top of the stack.
-     * @param newEntry The entry being added to the top of the stack.
-     */
+    //  < Implementations of the stack operations go here. >
+    // Ignore unexpected characters
+ 
+
     @Override
     public void push(T newEntry) {
         checkIntegrity();
@@ -79,10 +69,6 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>
         topIndex++;
     } // end push
 
-    
-    /** Removes the top of the stack and return the data of that element.
-     * @return T The data of the element at the top of the stack.
-     */
     @Override
     public T pop() {
         checkIntegrity();
@@ -96,9 +82,6 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>
         }
     } // end pop 
 
-    /** Remove the top of the stack and return the data of that element.
-     * @return T The data of the element at the top of the stack.
-     */
     @Override
     public T peek() {
         checkIntegrity();
@@ -109,95 +92,24 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>
         }
     } // end peek
 
-    
-    /** Checks if the stack is empty.
-     * @return boolean True if the stack is empty, false if not.
-     */
     @Override
     public boolean isEmpty() {
         return topIndex < 0;
     } // end isEmpty
 
-    /**
-     * Sets each array index of the stack to null, clearing the stack.
-     */
     @Override
     public void clear() {
         checkIntegrity();
+
+        // Remove refferences to the objects in the stack
+        // but do not deallocate the array
         while (topIndex > -1) {
             stack[topIndex] = null;
             topIndex--;
         }
         // Assertion: topIndex is -1
     } // end clear
+    // TODO Auto-generated method stub 
 
-    
-
-    public static double evaluePostfix(String postfixString) {
-       
-    StackInterface<Character> valueStackInterface = new ResizeableArrayStack<>() ;
-        String postfix = "" ;
-        char nextItem ;
-        int operand1 ;
-        int operand2 ;
-        int result = 0 ;
-        int i = 0 ;
-
-
-        while(i < postfix.length() ) {
-            nextItem = postfix.charAt(i) ;
-            if (charVariable(nextItem))
-                valueStackInterface.push(nextItem) ;
-            else {
-                operand1 = valueStackInterface.pop() ;
-                operand2 = valueStackInterface.pop() ;
-                switch(nextItem){
-                    case '+':result = operand1 + operand2;
-                    break;
-                    case '*':result = operand1 * operand2;
-                        break;
-                    case '-':result = operand1 - operand2;
-                        break;
-                    case '/':result = operand1 / operand2;
-                        break;
-                        default:break;
-                }
-                valueStackInterface.push(result);
-            }
-            i++ ;
-
-        }
-
-        
-        return valueStackInterface.pop() ;
-    }
-
-
-private int getPrecedence(char operator) {
-    int precedence = 0 ;
-    switch(operator){
-        case '*':
-            precedence = 2;
-            break;
-         case '/':
-            precedence = 2;
-            break;
-         case '+':
-            precedence = 1;
-            break;
-         case '-':
-            precedence = 1;
-            break;
-         default:
-            break;
-      }
-
-      return precedence;
-    }
-
-private static boolean charVariable(char ch){
-    return(ch == 'a') || (ch == 'b') || (ch == 'c') || (ch == 'd') ;
-}
-    
-    
-} // end ArrayStack
+ 
+         }
